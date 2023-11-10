0ffb31adf36b46ce6fe7855f53edc7434c1c2885
kokkermanz <89983730+kokkermanz@users.noreply.github.com>
2023-11-10 14:29:54 +0100

    Delete rinfox/Swedish directory

--- rinfox/Swedish/chrome/JS/move-personal-bar.uc.js
+++
@@ -1,9 +0,0 @@
-var MovePersonalBars = {
-  init: function() {
-	document.getElementById("titlebar").insertBefore(document.getElementById("PersonalToolbar"), document.getElementById("TabsToolbar"));
-	
-  }
-
-}
-
-document.addEventListener("DOMContentLoaded", MovePersonalBars.init(), false);
